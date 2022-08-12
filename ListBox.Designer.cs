eeb5e4b45bf12a33ea0ed90207c01f344c089f06
thea Villanueva <tei.villanueva@gmail.com>
2022-08-11 17:29:31 -0700

    Create a list box with add, remove, clear, exit features

---
+++ ListBox.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ListBoxApp
+{
+    partial class FrmListBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LstBox = new System.Windows.Forms.ListBox();
+            this.TxtInput = new System.Windows.Forms.TextBox();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnRemove = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LstBox
+            // 
+            this.LstBox.FormattingEnabled = true;
+            this.LstBox.ItemHeight = 16;
+            this.LstBox.Location = new System.Drawing.Point(110, 12);
+            this.LstBox.Name = "LstBox";
+            this.LstBox.Size = new System.Drawing.Size(219, 372);
+            this.LstBox.TabIndex = 0;
+            // 
+            // TxtInput
+            // 
+            this.TxtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtInput.Location = new System.Drawing.Point(400, 25);
+            this.TxtInput.Name = "TxtInput";
+            this.TxtInput.Size = new System.Drawing.Size(147, 30);
+            this.TxtInput.TabIndex = 1;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(429, 75);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(97, 48);
+            this.BtnAdd.TabIndex = 2;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnRemove
+            // 
+            this.BtnRemove.Location = new System.Drawing.Point(429, 142);
+            this.BtnRemove.Name = "BtnRemove";
+            this.BtnRemove.Size = new System.Drawing.Size(97, 52);
+            this.BtnRemove.TabIndex = 2;
+            this.BtnRemove.Text = "Remove";
+            this.BtnRemove.UseVisualStyleBackColor = true;
+            this.BtnRemove.Click += new System.EventHandler(this.BtnRemove_Click);
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(429, 213);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(97, 49);
+            this.BtnClear.TabIndex = 2;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(429, 281);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(97, 46);
+            this.BtnExit.TabIndex = 2;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // FrmListBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.BtnRemove);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.TxtInput);
+            this.Controls.Add(this.LstBox);
+            this.Name = "FrmListBox";
+            this.Text = "List Box ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LstBox;
+        private System.Windows.Forms.TextBox TxtInput;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnRemove;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Button BtnExit;
+    }
+}
+
---
+++ ListBox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxApp
+{
+    public partial class FrmListBox : Form
+    {
+        public FrmListBox()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+   
+            
+            //once add button gets clicked
+            //add the input from the textbox to display on the listbox
+            LstBox.Items.Add(TxtInput.Text);
+
+            TxtInput.Clear();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            //once clear is clicked, inputbox gets emptied
+            LstBox.Items.Clear();
+        }
+
+        private void BtnRemove_Click(object sender, EventArgs e)
+        {
+
+            // check the item selected, then remove
+            // -1 means none of the index were selected
+            if (LstBox.SelectedIndex != -1)
+            {
+                LstBox.Items.RemoveAt(LstBox.SelectedIndex);
+            }
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
